b7d8d83b02f77a7e936fd983ea504c080f51be2e
riotaro <rugby117117@gmail.com>
2026-01-21 05:42:13 +0900

    ルーティングの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { Route } from 'react-router';
+import { Routes } from 'react-router';
+import { BrowserRouter } from 'react-router';
+import Home from './page/Home';
+import Signin from './page/Signin';
+import Signup from './page/Signup';
+import ProtectedRoute from './Routes/ProtectedRoute';
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<ProtectedRoute/>}>
+          <Route path="/" element={<Home />} />
+        </Route>
+        <Route path='/signin' element={<Signin/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
