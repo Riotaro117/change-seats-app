9b6e74c398632f412a5b25bff1dfed84541b29dc
riotaro <rugby117117@gmail.com>
2026-01-22 06:15:03 +0900

    ボタンを共通化して取り出す

--- src/page/Signin.tsx
+++ src/page/Signin.tsx
@@ -1,9 +1,15 @@
-import { Link } from "react-router";
+import { useState } from 'react';
+import { Link } from 'react-router';
+import Button from '../components/Button';
 
 const Signin = () => {
-  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signin = () => {};
+
   return (
-      <div className="min-h-screen flex items-center justify-center bg-wood-50 p-4">
+    <div className="min-h-screen flex items-center justify-center bg-wood-50 p-4">
       <div className="bg-white p-8 rounded-3xl shadow-xl max-w-md w-full border-4 border-wood-200 text-center">
         <div className="w-20 h-20 bg-orange-100 rounded-full flex items-center justify-center mx-auto mb-6 text-4xl">
           🏫
@@ -13,7 +19,7 @@ const Signin = () => {
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            // signin();
+            signin();
           }}
           className="space-y-4"
         >
@@ -21,23 +27,20 @@ const Signin = () => {
             type="email"
             placeholder="メールアドレス"
             className="w-full px-4 py-3 rounded-xl border-2 border-wood-100 focus:border-wood-400 outline-none bg-wood-50"
-            // value={email}
-            // onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="password"
             placeholder="パスワード"
             className="w-full px-4 py-3 rounded-xl border-2 border-wood-100 focus:border-wood-400 outline-none bg-wood-50"
-            // value={password}
-            // onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          {/* <Button className="w-full py-3 text-lg" type="submit">
-            ログイン
-          </Button> */}
-
+          <Button type="submit">ログイン</Button>
         </form>
         <div className="mt-4 text-center text-sm">
-          <Link className="w-full underline" to={'/signup'}>
+          <Link className="w-full underline " to={'/signup'}>
             アカウントを新規作成
           </Link>
         </div>
