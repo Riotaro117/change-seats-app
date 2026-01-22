476f15e2cfaf0e284ae2e507d64e9cec99708d53
riotaro <rugby117117@gmail.com>
2026-01-23 07:43:23 +0900

    home調整

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,8 +1,17 @@
+import { useState } from 'react';
+import Header from '../components/layout/Header';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  // 画面表示の切り替え
+  const [viewMode, setViewMode] = useState('classroom');
 
-export default Home
+  const handleResize = () => {};
+  const signout = () => {};
+  return (
+    <div className="min-h-screen bg-wood-50 text-wood-900 pb-20 font-sans">
+    <Header/>
+    </div>
+  );
+};
+
+export default Home;
