e4b6b6d5b1552db63dee5f699eab226c01efdd59
riotaro <rugby117117@gmail.com>
2026-01-24 05:53:41 +0900

    Home調整

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,15 +1,38 @@
 import { useState } from 'react';
 import Header from '../components/layout/Header';
+import MainContents from '../components/home/MainContents';
+import SideButton from '../components/ui/SideButton';
+import { ChevronDown, ChevronUp } from 'lucide-react';
 
 const Home = () => {
   // 画面表示の切り替え
   const [viewMode, setViewMode] = useState('classroom');
+  const [totalSeats, setTotalSeats] = useState(30);
 
   const handleResize = () => {};
   const signout = () => {};
   return (
     <div className="min-h-screen bg-wood-50 text-wood-900 pb-20 font-sans">
-    <Header/>
+      <Header setViewMode={setViewMode} />
+      <MainContents />
+      <div className="fixed bottom-6 right-6 md:hidden">
+        <div className="bg-white p-2 rounded-full shadow-xl border-2 border-wood-200 flex flex-col gap-2">
+          <button
+            // onClick={() => handleResize(Math.min(40, totalSeats + 1))}
+            className="p-2 bg-wood-100 rounded-full hover:bg-wood-200"
+          >
+            <ChevronUp />
+          </button>
+          <span className="text-center font-bold text-xs">{totalSeats}</span>
+          {/* <span className="text-center font-bold text-xs">{30}</span> */}
+          <button
+            // onClick={() => handleResize(Math.max(20, totalSeats - 1))}
+            className="p-2 bg-wood-100 rounded-full hover:bg-wood-200"
+          >
+            <ChevronDown />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
