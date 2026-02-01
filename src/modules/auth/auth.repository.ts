750c7671d4e6875ad910eda86ecb701f252f24ba
riotaro <rugby117117@gmail.com>
2026-02-02 08:15:50 +0900

    ユーザー情報の取得機能作成

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -32,6 +32,14 @@ export const authRepository = {
     };
   },
   // ユーザー情報の取得
+  async getCurrentUser() {
+    const { data, error } = await supabase.auth.getUser();
+    if (data.user == null || error != null) throw new Error(error?.message);
+    return {
+      ...data.user,
+      userName: data.user.user_metadata.name,
+    };
+  },
   // ログイン、ログアウト、トークン更新などのリアルタイム変化を追従
   // ログアウト機能
 };
