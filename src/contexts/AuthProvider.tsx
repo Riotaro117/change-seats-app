9a8245e20d0fdf5d85f1dfe170c2ff82e78507b1
riotaro <rugby117117@gmail.com>
2026-02-03 06:05:49 +0900

    AuthProviderの作成

--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,20 +1,33 @@
 import { useEffect, useState } from 'react';
 import { useCurrentUserStore } from '../modules/auth/current-user.state';
 import { authRepository } from '../modules/auth/auth.repository';
+import { AuthContext } from './AuthContext';
 
-export type AuthContextType = {};
+export type AuthContextType = {
+  isLoading: boolean;
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const { set } = useCurrentUserStore();
+  const { setUser } = useCurrentUserStore();
   const [isLoading, setIsLoading] = useState(false);
   // 初回の信頼できるユーザーのみ読み込み
   useEffect(() => {
     const getUser = async () => {
       setIsLoading(true);
       const currentUser = await authRepository.getCurrentUser();
-      set(currentUser);
+      setUser(currentUser);
       setIsLoading(false);
     };
     getUser();
+
+    const unSubscribe = authRepository.stateChange((user) => {
+      setUser(user ?? undefined);
+    });
+    return unSubscribe;
   }, []);
+
+  return (
+    <AuthContext.Provider value={{ isLoading, setIsLoading }}>{children}</AuthContext.Provider>
+  );
 };
