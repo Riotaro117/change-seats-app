4ddf31f29a97e0e42e580026fd3dfb3893cdb694
riotaro <rugby117117@gmail.com>
2026-02-03 06:07:16 +0900

    ログイン状態の監視を作成

--- src/Routes/ProtectedRoute.tsx
+++ src/Routes/ProtectedRoute.tsx
@@ -1,7 +1,21 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from 'react-router';
+import { useAuth } from '../contexts/useAuth';
+import { Loader2 } from 'lucide-react';
+import { useCurrentUserStore } from '../modules/auth/current-user.state';
 
-const ProtectedRoute = () => {
-  return <Outlet/>
+export const ProtectedRoute = () => {
+  const { isLoading } = useAuth();
+  const { currentUser } = useCurrentUserStore();
+
+  if (isLoading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-wood-50">
+        <Loader2 className="w-10 h-10 animate-spin text-wood-500" />
+      </div>
+    );
+  }
+  if (!currentUser) {
+    return <Navigate replace to="/signin" />;
+  }
+  return <Outlet />;
 };
-
-export default ProtectedRoute;
