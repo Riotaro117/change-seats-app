74d6594bf558eb5e3f81aceea08af99e0d6471f8
riotaro <rugby117117@gmail.com>
2026-02-03 06:05:04 +0900

    コンポーネントの一旦廃止

--- src/components/home/MainContents.tsx
+++ src/components/home/MainContents.tsx
@@ -1,14 +1,10 @@
-import ClassroomButton from '../ui/ClassroomButton';
+// // import ClassroomButton from '../ui/ClassroomButton';
 
-const MainContents = () => {
-  // 画面表示の切り替え
-  return (
-    <main className="max-w-7xl mx-auto px-4 pt-6">
-        <>
-          <ClassroomButton/>
-        </>
-    </main>
-  );
-};
+// const MainContents = () => {
+//   // 画面表示の切り替え
+//   return (
+  
+//   );
+// };
 
-export default MainContents;
+// export default MainContents;
