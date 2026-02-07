7ec7dde6b6757774918deebefed51722facb938e
riotaro <rugby117117@gmail.com>
2026-02-08 06:04:41 +0900

    studentsのグローバルステートの修正

--- src/modules/students/students.state.ts
+++ src/modules/students/students.state.ts
@@ -1,7 +1,7 @@
 import { atom, useAtom } from 'jotai';
 import type { Student } from '../../type';
 
-const studentsAtom = atom<Student[] | null>([]);
+const studentsAtom = atom<Student[]>([]); // 未取得だとnullだが今回は区別する
 
 export const useStudentsStore = () => {
   const [students, setStudents] = useAtom(studentsAtom);
