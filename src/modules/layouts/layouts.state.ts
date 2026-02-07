1efa5f750f4977563bfa963925d2425416392a8f
riotaro <rugby117117@gmail.com>
2026-02-08 06:04:58 +0900

    layoutsのグローバルステートの修正

--- src/modules/layouts/layouts.state.ts
+++ src/modules/layouts/layouts.state.ts
@@ -1,7 +1,7 @@
 import { atom, useAtom } from 'jotai';
 import type { ClassroomLayout } from '../../type';
 
-const LayoutsAtom = atom<ClassroomLayout[] | null>([]);
+const LayoutsAtom = atom<ClassroomLayout[]>([]);
 export const useLayoutsStore = () => {
   const [layouts, setLayouts] = useAtom(LayoutsAtom);
   return { layouts, setLayouts };
