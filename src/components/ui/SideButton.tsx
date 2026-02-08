ac422b2c13242949a428b7daf4c9489b4ed445e2
riotaro <rugby117117@gmail.com>
2026-02-08 13:34:43 +0900

    cursor-pointerの追加

--- src/components/ui/SideButton.tsx
+++ src/components/ui/SideButton.tsx
@@ -11,14 +11,14 @@ const SideButton: React.FC<SideButtonProps> = ({ onResize, totalSeats }) => {
       <div className="bg-white p-2 rounded-full shadow-xl border-2 border-wood-200 flex flex-col gap-2">
         <button
           onClick={() => onResize(Math.min(40, totalSeats + 1))}
-          className="p-2 bg-wood-100 rounded-full hover:bg-wood-200"
+          className="cursor-pointer p-2 bg-wood-100 rounded-full hover:bg-wood-200"
         >
           <ChevronUp />
         </button>
         <span className="text-center font-bold text-xs">{totalSeats}</span>
         <button
           onClick={() => onResize(Math.max(20, totalSeats - 1))}
-          className="p-2 bg-wood-100 rounded-full hover:bg-wood-200"
+          className="cursor-pointer p-2 bg-wood-100 rounded-full hover:bg-wood-200"
         >
           <ChevronDown />
         </button>
