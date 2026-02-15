aab30aa190fcb2b9640fa2ccd29a2babeba6d268
riotaro <rugby117117@gmail.com>
2026-02-16 04:54:25 +0900

    getCurrentUserの削除

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -32,15 +32,7 @@ export const authRepository = {
       userName: data.user.user_metadata.name,
     };
   },
-  // ユーザー情報の取得
-  async getCurrentUser() {
-    const { data, error } = await supabase.auth.getUser();
-    if (data.user == null || error != null) throw new Error(error?.message);
-    return {
-      ...data.user,
-      userName: data.user.user_metadata.name,
-    };
-  },
+
   // ログイン、ログアウト、トークン更新などのリアルタイム変化を追従
   stateChange(onChange: (user: User | null) => void) {
     const {
