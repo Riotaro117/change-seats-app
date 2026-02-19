50184f27a403ad6f38267da21fdc4c01453e1e60
riotaro <rugby117117@gmail.com>
2026-02-20 05:24:51 +0900

    型のnullの追加

--- src/modules/auth/current-user.state.ts
+++ src/modules/auth/current-user.state.ts
@@ -1,7 +1,7 @@
 import type { User } from '@supabase/supabase-js';
 import { atom, useAtom } from 'jotai';
 
-const currentUserAtom = atom<User>();
+const currentUserAtom = atom<User | null>(null);
 
 export const useCurrentUserStore = () => {
   const [currentUser, setCurrentUser] = useAtom(currentUserAtom);
