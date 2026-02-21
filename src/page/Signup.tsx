b76480e20fa6c40578274a49150669be497f5514
riotaro <rugby117117@gmail.com>
2026-02-22 05:43:19 +0900

    登録時の挙動を修正

--- src/page/Signup.tsx
+++ src/page/Signup.tsx
@@ -1,16 +1,14 @@
-import { useEffect, useState } from 'react';
-import { Link, Navigate, useNavigate } from 'react-router';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router';
 import Button from '../components/ui/Button';
 import { authRepository } from '../modules/auth/auth.repository';
 import { useAuth } from '../contexts/useAuth';
-import { useCurrentUserStore } from '../modules/auth/current-user.state';
 
 const Signup = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { isLoading, setIsLoading } = useAuth();
-  const { currentUser } = useCurrentUserStore();
   const navigate = useNavigate();
 
   const signup = async () => {
@@ -18,27 +16,21 @@ const Signup = () => {
     setIsLoading(true);
     try {
       await authRepository.signup(email, password, name);
+      alert('登録ありがとうございます！');
+      navigate('/registered', { replace: true });
     } catch (error) {
       console.error(error);
-      alert('入力欄に必須事項を入力して下さい。');
+      alert('既に登録されているメールアドレスか存在しないメールアドレスの可能性があります');
     } finally {
       setIsLoading(false);
     }
   };
 
-  useEffect(() => {
-    if (currentUser) {
-      navigate('/', { replace: true });
-    }
-  }, [currentUser]);
-
-  if (currentUser) return <Navigate replace to="/" />;
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-wood-50 p-4">
       <div className="bg-white p-8 rounded-3xl shadow-xl max-w-md w-full border-4 border-wood-200 text-center">
         <h1 className="text-3xl font-bold text-wood-800 font-serif mb-2">Seat Tree</h1>
-        <p className="text-wood-500 mb-8">-配慮できる席替えアプリ-</p>
+        <p className="text-wood-500 mb-8 font-serif">-配慮できる席替えアプリ-</p>
         <p className="text-wood-500 mb-8">以下の情報を入力し、ユーザーを登録して下さい。</p>
         <form
           onSubmit={(e) => {
@@ -68,10 +60,13 @@ const Signup = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <Button disabled={!name || !email || !password} type="submit">
+          <Button disabled={!name || !email || !password || isLoading} type="submit">
             ユーザー登録してログイン
           </Button>
         </form>
+        <p className="text-wood-500 mt-8">
+          メールアドレスに確認メールが届くので、メールを認証でき次第ログインができます。
+        </p>
         <div className="mt-4 text-center text-sm">
           <Link className="w-full underline" to={'/signin'}>
             すでにアカウントをお持ちの方
