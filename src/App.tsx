f847dd0259a14a40ca106133a4d678884c324de3
riotaro <rugby117117@gmail.com>
2026-02-22 05:43:56 +0900

    Routeの整理

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,28 @@ import Signin from './page/Signin';
 import Signup from './page/Signup';
 import { ProtectedRoute } from './Routes/ProtectedRoute';
 import UpdateUser from './page/UpdateUser';
+import { AnonymousOnlyRoute } from './Routes/AnonymousOnlyRoute';
+import PublicOnlyRoute from './Routes/PublicOnlyRoute';
+import Registered from './page/Registered';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* 本ユーザー専用 */}
         <Route element={<ProtectedRoute />}>
           <Route path="/" element={<Home />} />
         </Route>
-        <Route path="/signin" element={<Signin />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/updateUser" element={<UpdateUser />}></Route>
+        {/* 仮ユーザー専用 */}
+        <Route element={<AnonymousOnlyRoute />}>
+          <Route path="/updateUser" element={<UpdateUser />} />
+        </Route>
+        {/* 未ログイン専用 */}
+        <Route element={<PublicOnlyRoute />}>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/registered" element={<Registered />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
