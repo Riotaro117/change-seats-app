f3adcd6d34ffd69aeec735f958dde5f0317c4c2e
riotaro <rugby117117@gmail.com>
2026-02-25 06:13:49 +0900

    Excelテンプレートダウンロードリンクをインポートコンポーネントに追加

--- src/components/ui/ImportExcelFile.tsx
+++ src/components/ui/ImportExcelFile.tsx
@@ -3,6 +3,7 @@ const ImportExcelFile = () => {
   return (
           <div className="flex flex-col sm:flex-row gap-2 mb-6 bg-wood-50 p-4 rounded-xl border border-wood-100">
           <p>Excelファイルをインポートして追加する</p>
+          <a href="">Excelテンプレートをダウンロード</a>
           </div>
   )
 }
