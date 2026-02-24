e0fdb8df7a3ba6b267a01ca36269833c2108547a
riotaro <rugby117117@gmail.com>
2026-02-25 05:11:51 +0900

    Excelファイルインポートコンポーネントの追加

---
+++ src/components/ui/ImportExcelFile.tsx
@@ -0,0 +1,10 @@
+
+const ImportExcelFile = () => {
+  return (
+          <div className="flex flex-col sm:flex-row gap-2 mb-6 bg-wood-50 p-4 rounded-xl border border-wood-100">
+          <p>Excelファイルをインポートして追加する</p>
+          </div>
+  )
+}
+
+export default ImportExcelFile
