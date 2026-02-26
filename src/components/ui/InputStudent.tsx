3cd1400d3e1e8663acdd88d265282d4e124702e5
riotaro <rugby117117@gmail.com>
2026-02-27 07:48:49 +0900

    margin-bottomの削除

--- src/components/ui/InputStudent.tsx
+++ src/components/ui/InputStudent.tsx
@@ -40,7 +40,7 @@ const InputStudent: React.FC = () => {
     }
   };
   return (
-    <div className="flex flex-col sm:flex-row gap-2 mb-6 bg-wood-50 p-4 rounded-xl border border-wood-100">
+    <div className="flex flex-col sm:flex-row gap-2 bg-wood-50 p-4 rounded-xl border border-wood-100">
       <input
         type="text"
         value={newStudentName}
