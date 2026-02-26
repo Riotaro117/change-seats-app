18160b98e5563a003bf972f4e8f627ae70c27344
riotaro <rugby117117@gmail.com>
2026-02-27 07:48:23 +0900

    文章の記述の変更

--- src/components/ui/ImportExcelFile.tsx
+++ src/components/ui/ImportExcelFile.tsx
@@ -49,8 +49,7 @@ const ImportExcelFile = () => {
     }
   };
   return (
-    <div className="flex flex-col sm:flex-row gap-2 mb-6 bg-wood-50 p-4 rounded-xl border border-wood-100">
-      <p>Excelファイルをインポートして追加する</p>
+    <div className="flex flex-col sm:flex-row gap-2 bg-wood-50 p-4 rounded-xl border border-wood-100">
       <a href="/students_name_template.xlsx" download>
         Excelテンプレートをダウンロード
       </a>
